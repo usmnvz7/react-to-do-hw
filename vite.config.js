3965f32646652863c8aaf436cbb3571a104c0181
atham <atham1@gmail.com>
2026-03-31 19:38:59 +0500

    done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-to-do-hw/', // Название твоего репозитория
+  base: '/react-to-do-hw1/', // Название твоего репозитория
   plugins: [react()],
 })
