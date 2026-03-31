60fae4f9c0db6cf4a54973b8c25483256fe6bd53
adadadadadaddaad <adadada2@gmail.com>
2026-03-31 19:48:04 +0500

    done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-to-do-hw1/', // Название твоего репозитория
   plugins: [react()],
 })
